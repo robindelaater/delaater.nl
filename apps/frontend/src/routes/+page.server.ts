944e84ff6bfbcbfe1925b188681a6ab5cb32f5d2
Robin de Laater <robin@suikerspin.studio>
2026-05-13 13:09:32 +0200

    feat: add resend integration

--- apps/frontend/src/routes/+page.server.ts
+++ apps/frontend/src/routes/+page.server.ts
@@ -1,6 +1,8 @@
 import { serverClient } from '$lib/cms/client.server';
 import { homePageQuery } from '$lib/cms/queries';
 import type { Actions } from '@sveltejs/kit';
+import { Resend } from 'resend';
+import { env } from '$env/dynamic/private';
 
 export const load = async () => {
   const data = await serverClient.fetch(homePageQuery);
@@ -13,12 +15,26 @@ export const actions: Actions = {
 	default: async ({ request }) => {
 		const formData = await request.formData();
 
-		const name = formData.get('name');
+		const firstname = formData.get('first_name');
+		const lastname = formData.get('last_name');
+		const company = formData.get('company');
 		const email = formData.get('email');
 		const date = formData.get('date');
-		const time = formData.get('time');
 
-		console.log(name, email, date, time)
+		const resend = new Resend(env.RESEND_API_KEY)
+		await resend.emails.send({
+			to: 'robin@laater.dev',
+			template: {
+				id: 'new-inquiry',
+				variables: {
+					firstname: firstname || '',
+					lastname: lastname || '',
+					company: company || '',
+					email: email || '',
+					date: date || '',
+				}
+			}
+		})
 
 		return {
 			success: true
